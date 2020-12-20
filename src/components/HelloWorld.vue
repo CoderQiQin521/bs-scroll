05a19b94928c9d47ee58f879945c465d077b433c
CoderQiQin521 <coderqiqin@aliyun.com>
2020-12-20 15:59:07 +0800

    ✨feat: 封装滚动组件 & 调用上拉加载

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,19 +2,26 @@
   <div class="hello">
     <button @click="switchTab(1)">在职</button>
     <button @click="switchTab(2)">离职</button>
-    <div class="wrapper" ref="wrapper">
+    <!-- :pulldown="true"
+      @pulldown="loadData" -->
+    <scroll
+      class="wrapper"
+      :data="list"
+      :pullup="true"
+      @scrollToEnd="scrollEnd"
+    >
       <ul class="content">
         <li class="item" v-for="item in list" :key="item.id" @click="handle">
           {{ item.name }}
         </li>
       </ul>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import BetterScroll from "better-scroll";
+import scroll from "./better-scroll";
 
 export default {
   name: "HelloWorld",
@@ -32,55 +39,34 @@ export default {
   created() {
     this.loadData();
   },
+  components: {
+    scroll
+  },
   mounted() {},
   methods: {
+    scrollEnd() {
+      console.log("this.pageInfo: ", this.pageInfo);
+      console.log("this.page: ", this.page);
+      // 接口还有数据的话,才继续请求
+      if (this.page < this.pageInfo.last_page) {
+        this.page++;
+        this.loadData();
+      } else {
+        console.log("没有数据了");
+      }
+    },
     loadData(isSwitch) {
-      axios
-        .get(
-          `http://192.168.2.159:3001/book/list?is_quit=${
-            this.type === 1 ? 0 : 2
-          }&page=${this.page}`
-        )
-        .then(res => {
-          let data = res.data.data;
-          this.pageInfo = data;
-          // 切换重新复制     如果没有数据  不操作  判断
-          if (data.data.length) {
-            this.list = isSwitch ? data.data : this.list.concat(data.data);
-          }
-          // console.log("data.data: ", data.data);
-          // DOM渲染完成,正确计算高度,确保滚动正常
-          // !Vue 数据发生变化（this.data = res.data）到页面重新渲染是一个异步的过程，我们的初始化时机是要在 DOM 重新渲染后，所以这里用到了 this.$nextTick，当然替换成 setTimeout(fn, 20) 也是可以的。
-          this.$nextTick(() => {
-            if (!this.scroll) {
-              this.scroll = new BetterScroll(this.$refs.wrapper, {
-                click: true,
-                pullUpLoad: true // 上拉加载
-                // probeType: 3
-              });
-              this.scroll.on("touchEnd", pos => {
-                if (pos > 50) {
-                  console.log("touchEnd");
-                  alert("touchEnd");
-                  this.loadData();
-                }
-              });
-              this.scroll.on("pullingUp", () => {
-                console.log("pullingUp");
-                // 接口还有数据的话,才继续请求
-                if (this.page < this.pageInfo.last_page) {
-                  this.page++;
-                  this.loadData();
-                } else {
-                  console.log("没有数据了");
-                }
-                this.scroll.finishPullUp();
-              });
-            } else {
-              this.scroll.refresh();
-            }
-          });
-        });
+      let url = `http://192.168.2.159:3001/book/list?is_quit=${
+        this.type === 1 ? 0 : 2
+      }&page=${this.page}`;
+      axios.get(url).then(res => {
+        let data = res.data.data;
+        this.pageInfo = data;
+        // 切换重新复制     如果没有数据  不操作  判断
+        if (this.page <= this.pageInfo.last_page) {
+          this.list = isSwitch ? data.data : this.list.concat(data.data);
+        }
+      });
     },
     switchTab(type) {
       if (this.type === type) {
