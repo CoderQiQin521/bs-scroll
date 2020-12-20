e8cd61a36f3b9e26b915944d9db464e48780dcab
CoderQiQin521 <coderqiqin@aliyun.com>
2020-12-20 11:16:11 +0800

    ✨feat: 实现上拉加载与切换,性能待优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
-    <button @click="loadData(1)">分类1</button>
-    <button @click="loadData(2)">分类2</button>
+    <button @click="switchTab(1)">在职</button>
+    <button @click="switchTab(2)">离职</button>
     <div class="wrapper" ref="wrapper">
       <ul class="content">
-        <li class="item" v-for="item in list" :key="item" @click="handle">
-          {{ item }}
+        <li class="item" v-for="item in list" :key="item.id" @click="handle">
+          {{ item.name }}
         </li>
       </ul>
     </div>
@@ -23,7 +23,10 @@ export default {
   },
   data() {
     return {
-      list: []
+      list: [],
+      type: 1,
+      page: 1,
+      pageInfo: {}
     };
   },
   created() {
@@ -31,23 +34,65 @@ export default {
   },
   mounted() {},
   methods: {
-    loadData(type = 1) {
-      axios.get("http://localhost:3001/book/list?type=" + type).then(res => {
-        let data = res.data;
-        this.list = data.data;
-        console.log("data.data: ", data.data);
-        // DOM渲染完成,正确计算高度,确保滚动正常
-        // !Vue 数据发生变化（this.data = res.data）到页面重新渲染是一个异步的过程，我们的初始化时机是要在 DOM 重新渲染后，所以这里用到了 this.$nextTick，当然替换成 setTimeout(fn, 20) 也是可以的。
-        this.$nextTick(() => {
-          if (!this.scroll) {
-            this.scroll = new BetterScroll(this.$refs.wrapper, {
-              click: true
-            });
-          } else {
-            this.scroll.refresh();
+    loadData(isSwitch) {
+      axios
+        .get(
+          `http://192.168.2.159:3001/book/list?is_quit=${
+            this.type === 1 ? 0 : 2
+          }&page=${this.page}`
+        )
+        .then(res => {
+          let data = res.data.data;
+          this.pageInfo = data;
+          // 切换重新复制     如果没有数据  不操作  判断
+          if (data.data.length) {
+            this.list = isSwitch ? data.data : this.list.concat(data.data);
           }
+          // console.log("data.data: ", data.data);
+          // DOM渲染完成,正确计算高度,确保滚动正常
+          // !Vue 数据发生变化（this.data = res.data）到页面重新渲染是一个异步的过程，我们的初始化时机是要在 DOM 重新渲染后，所以这里用到了 this.$nextTick，当然替换成 setTimeout(fn, 20) 也是可以的。
+          this.$nextTick(() => {
+            if (!this.scroll) {
+              this.scroll = new BetterScroll(this.$refs.wrapper, {
+                click: true,
+                pullUpLoad: true // 上拉加载
+                // probeType: 3
+              });
+              this.scroll.on("touchEnd", pos => {
+                if (pos > 50) {
+                  console.log("touchEnd");
+                  alert("touchEnd");
+                  this.loadData();
+                }
+              });
+              this.scroll.on("pullingUp", () => {
+                console.log("pullingUp");
+                // 接口还有数据的话,才继续请求
+                if (this.page < this.pageInfo.last_page) {
+                  this.page++;
+                  this.loadData();
+                } else {
+                  console.log("没有数据了");
+                }
+                this.scroll.finishPullUp();
+              });
+            } else {
+              this.scroll.refresh();
+            }
+          });
         });
+    },
+    switchTab(type) {
+      if (this.type === type) {
+        return;
+      }
+      this.type = type;
+      this.page = 1;
+      console.log("switchTab: ", {
+        type: this.type,
+        page: this.page
       });
+      this.loadData(true);
     },
     handle() {
       alert(1);
